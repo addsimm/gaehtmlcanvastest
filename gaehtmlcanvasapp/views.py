0d45a805809a17a1b88d3efee8beee0cd600a719
addsimm <adamfsimon@gmail.com>
2015-04-30 10:41:33 -0700

    Added bootstrap and base html templates

--- gaehtmlcanvasapp/views.py
+++ gaehtmlcanvasapp/views.py
@@ -1,21 +1,11 @@
 # Create your views here.
-
-
 from __future__ import print_function
 
-from django.core.context_processors import csrf
-from django.shortcuts import render_to_response, redirect
-from django.core.validators import validate_email
-
-from google.appengine.api import users
-from google.appengine.api import mail  # --enable_
-
-from gaehtmlcanvasapp.models import *
-
+from django.shortcuts import render_to_response
 
 def index(request):
-    template_values = {}
-    template_values.update({'page_name': 'Landing Page'
-                            })
+   template_values = {}
+   template_values.update({'page_name': 'Landing Page'
+                           })
 
-    return render_to_response('base.html', template_values)
+   return render_to_response('index.html', template_values)
